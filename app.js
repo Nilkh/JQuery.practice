884b6ed042b01a78f2d2a3073b7bcfa35a0c36ad
Nilkh <khsrawynilofar@gmail.com>
2023-06-06 11:18:36 -0600

    add fav food and colors in app.js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 $(document).ready(() => {
-console.log("i am working");
+	//grabs the element with ID starting-point.
+	 const startingElement = $('#starting-point');
+	console.log("starting-point");
+	//creat a new dic.
+	const siblingElement = $('<div></div>');
+	//add test tp the new div
+	siblingElement.text('this is the next page');
+	//appends thr new div to startingElement
+	startingElement.after(siblingElement)
+	const favoriteFoods = $('#favorite-Foods');
+    console.log(favoriteFoods);
+    favoriteFoods.css('background-color', 'red')
+    //console.log(favoriteFoods[0].innerHTML)
+    //console.log(favoriteFoods)[0];
+	// code reusability.
+//     const favs = [
+// 	{ food: 'pizza', color: 'green' },
+// 	{ food: 'falafel', color: 'yellow' },
+// 	{ food: 'bolani', color: 'red' },
+// 	{ food: 'hot dog', color: 'orange' },
+// ] 
+    
+
+// favs.forEach(obj => {
+//     let favFoods = $("<div></div>")
+//     .text(obj.food)
+//     .css('background-color', obj.color)
+//     favoriteFoods.after(favFoods);
+// })
 });
